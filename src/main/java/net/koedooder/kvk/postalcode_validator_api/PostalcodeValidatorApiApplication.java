236e687dd7d8da47c219be3925a690bbf10d17f7
Ruud Koedoodr <ruud@koedooder.net>
2025-08-11 13:21:25 +0200

    added spring initializer starter code

---
+++ src/main/java/net/koedooder/kvk/postalcode_validator_api/PostalcodeValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package net.koedooder.kvk.postalcode_validator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalcodeValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalcodeValidatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/koedooder/kvk/postalcode_validator_api/PostalcodeValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.koedooder.kvk.postalcode_validator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalcodeValidatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
