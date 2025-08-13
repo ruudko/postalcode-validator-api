d2cf84a916d69e671e3f1c91c2e2f40e333c2ff7
Ruud Koedoodr <ruud@koedooder.net>
2025-08-13 11:16:11 +0200

    changed data object name

--- src/main/java/net/koedooder/kvk/pva/repository/PostalCodeRepository.java
+++ src/main/java/net/koedooder/kvk/pva/repository/PostalCodeRepository.java
@@ -1,10 +1,10 @@
 package net.koedooder.kvk.pva.repository;
 
-import net.koedooder.kvk.pva.model.PostalCode;
+import net.koedooder.kvk.pva.model.PostalCodeValidator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PostalCodeRepository extends JpaRepository<PostalCode, Long> {
-    Optional<PostalCode> findByCountryCode(String countryCode);
+public interface PostalCodeRepository extends JpaRepository<PostalCodeValidator, Long> {
+    Optional<PostalCodeValidator> findByCountryCode(String countryCode);
 }
